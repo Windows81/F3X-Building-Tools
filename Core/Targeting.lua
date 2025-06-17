c050888d7a357d70eef6bc3a6cc97493ae951dbd
Windows81 <nastyahmede@gmail.com>
2025-06-17 06:55:59 +0000

    2025-06-17T0656Z

--- Core/Targeting.lua
+++ Core/Targeting.lua
@@ -139,7 +139,6 @@ function TargetingModule:UpdateTarget(Scope, Force)
 				TextStrokeTransparency = 0;
 				TextSize = 8;
 				BackgroundTransparency = 1;
-				--		Position = UDim2.new(0, Mouse.X + 20, 0, Mouse.Y - 26);
 				AutomaticSize = Enum.AutomaticSize.XY;
 			};
 		end
